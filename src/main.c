f0e84cd0b8a56ef4a1fa414e0fba600575fa4296
valentin <valentinvacher01@gmail.com>
2022-06-28 13:49:45 +0200

    test

--- src/main.c
+++ src/main.c
@@ -1,10 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <SDL.h>
+#include <SDL/SDL.h>
 
 int main(int argc, char *argv[])
 {
+    SDL_Surface *ecran = NULL, *menu = NULL;
 
+    SDL_Rect positionMenu;
 
-    return 0
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_WM_SetIcon(IMG_Load("link.bmp"), NULL);
+    ecran = SDL_SetVideoMode(952, 442, 64, SDL_HWSurface);
+
+    return 0;
 }
